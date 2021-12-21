e9f0af52d7ac175bf274e94a534ce88f285f873c
red-Pen9uin <red.pen9uin@gmail.com>
2021-12-21 15:42:18 +0900

    CPP Solution of level 1 is Updated.

---
+++ Baekjoon/입출력과 사칙연산/1008_A_div_B/red-Pen9uin.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+        int a,b;
+        cin >> a >> b;
+//      cout.unsetf(ios::fixed);
+//      cout << (double)a/b;
+        printf("%.10lf", (double)a/b);
+
+        return 0;
+}
---
+++ Baekjoon/입출력과 사칙연산/2588_곱셈/red-Pen9uin.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+        int num1, num2;
+        int n1, n2, n3;
+        int result;
+
+        cin >> num1 >> num2;
+
+        result = num1*num2;
+
+        n1 = num2/100;
+        num2 %=100;
+        n2 = num2/10;
+        n3 = num2%10;
+
+        cout << num1*n3 << '\n';
+        cout << num1*n2 << '\n';
+        cout << num1*n1 << '\n';
+        cout << result << '\n';
+
+        return 0;
+}
